361ee7a51fddf4247c32e2e3c0d3a385af22c104
Shane99dt <dushen1999thilaksha@gmail.com>
2022-09-12 21:03:47 +0200

    backend live try

--- src/api/articles.js
+++ src/api/articles.js
@@ -1,45 +1,57 @@
 const getArticles = async () => {
-  const request = await fetch('http://localhost:5000/articles/')
-  const response = await request.json()
-  return response
-}
+  const request = await fetch("https://miniblog-dt.herokuapp.com/articles/");
+  const response = await request.json();
+  return response;
+};
 
 const getFilteredArticles = async (slug) => {
-  const request = await fetch(`http://localhost:5000/articles/categories/${slug}`)
-  const response = await request.json()
-  return response
-}
-
+  const request = await fetch(
+    `https://miniblog-dt.herokuapp.com/articles/categories/${slug}`
+  );
+  const response = await request.json();
+  return response;
+};
 
 const getArticle = async (slug, slugArticle) => {
-  const request = await fetch(`http://localhost:5000/articles/categories/${slug}/${slugArticle}`)
-  const response = await request.json()
-  return response
-}
+  const request = await fetch(
+    `https://miniblog-dt.herokuapp.com/articles/categories/${slug}/${slugArticle}`
+  );
+  const response = await request.json();
+  return response;
+};
 
 const postArticle = async (slug, body) => {
-  const request = await fetch(`http://localhost:5000/articles/categories/${slug}`, {
-    method: "POST",
-    headers: {'Content-Type':'application/json'},
-    body: JSON.stringify(body)
-  })
-  console.log(request.status)
-  const response = await request.json()
+  const request = await fetch(
+    `https://miniblog-dt.herokuapp.com/articles/categories/${slug}`,
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(body),
+    }
+  );
+  console.log(request.status);
+  const response = await request.json();
 
-  return{
+  return {
     status: request.status,
-    response
-  }
-}
-
+    response,
+  };
+};
 
 const deleteArticle = async (slug, slugArticle) => {
-  const request = await fetch(`http://localhost:5000/articles/categories/${slug}/${slugArticle}`, {
-    method:"DELETE"
-  })
-  return request
-}
+  const request = await fetch(
+    `https://miniblog-dt.herokuapp.com/articles/categories/${slug}/${slugArticle}`,
+    {
+      method: "DELETE",
+    }
+  );
+  return request;
+};
 
-
-
-export { getArticles, getFilteredArticles, getArticle, postArticle, deleteArticle }
+export {
+  getArticles,
+  getFilteredArticles,
+  getArticle,
+  postArticle,
+  deleteArticle,
+};
--- src/api/categories.js
+++ src/api/categories.js
@@ -1,22 +1,21 @@
 const getCategories = async () => {
-  const request = await fetch('http://localhost:5000/categories/')
-  const response = await request.json()
-  return response
-}
+  const request = await fetch("https://miniblog-dt.herokuapp.com/categories/");
+  const response = await request.json();
+  return response;
+};
 
 const postCategory = async (body) => {
-  const request = await fetch(`http://localhost:5000/categories`, {
+  const request = await fetch(`https://miniblog-dt.herokuapp.com/categories`, {
     method: "POST",
-    headers: {'Content-Type':'application/json'},
-    body: JSON.stringify(body)
-  })
-  const response = await request.json()
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(body),
+  });
+  const response = await request.json();
 
-  return{
+  return {
     status: request.status,
-    response
-  }
-}
+    response,
+  };
+};
 
-
-export { getCategories, postCategory }
+export { getCategories, postCategory };
