3dca885d1d2f4268c68b7f8e0128bccd8bcf521d
Shane99dt <dushen1999thilaksha@gmail.com>
2022-09-12 21:29:26 +0200

    backend restricted adding new

--- src/App.js
+++ src/App.js
@@ -1,32 +1,37 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Container from './components.js/Container'
-import Navbar from './components.js/Navbar'
-import Article from './pages/Article'
-import Articles from './pages/Articles'
-import ArticlesFiltered from './pages/ArticlesFiltered'
-import Categories from './pages/Categories'
-import Home from './pages/Home'
-import NewArticle from './pages/NewArticle'
-import NewCategory from './pages/NewCategory'
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Container from "./components.js/Container";
+import Navbar from "./components.js/Navbar";
+import Article from "./pages/Article";
+import Articles from "./pages/Articles";
+import ArticlesFiltered from "./pages/ArticlesFiltered";
+import Categories from "./pages/Categories";
+import Home from "./pages/Home";
+import NewArticle from "./pages/NewArticle";
+import NewCategory from "./pages/NewCategory";
 
 const App = () => {
-  return(
+  return (
     <BrowserRouter>
-      <Navbar/>
+      <Navbar />
       <Container>
         <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/categories" element={<Categories/>}/>
-          <Route path="/articles" element={<Articles/>}/>
-          <Route path="/articles/categories/:slug" element={<ArticlesFiltered/>}/>
-          <Route path="/articles/categories/:slug/:slugArticle" element={<Article/>}/>
-          <Route path="/new-article" element={<NewArticle/>}/>
-          <Route path="/new-category" element={<NewCategory/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/categories" element={<Categories />} />
+          <Route path="/articles" element={<Articles />} />
+          <Route
+            path="/articles/categories/:slug"
+            element={<ArticlesFiltered />}
+          />
+          <Route
+            path="/articles/categories/:slug/:slugArticle"
+            element={<Article />}
+          />
+          {/* <Route path="/new-article" element={<NewArticle/>}/>
+          <Route path="/new-category" element={<NewCategory/>}/> */}
         </Routes>
       </Container>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
